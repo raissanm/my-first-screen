7d7229b9d7e3e46bae5cfd34ef905e01645984bf
raissanm <raissanamo1234@gmail.com>
2024-10-07 21:58:47 -0300

    Trabalhando com Hook useState

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
+  let [newTask, setNewTask] = React.useState('');
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Cadastro </Text >
-      <Text>Digite seu nome</Text>
-      <TextInput placeholder='Nome'  style={styles.textinp}/>
+      <Text>Trabalhando com Hook useState</Text>
+      <TextInput onChangeText={setNewTask} placeholder='Tarefas' style={styles.textinp}/>
+      <Text></Text>
+      <Text>Elemento digitado:</Text>
+      <Text>{newTask}</Text>
+
+      {/*<Text style={styles.text}>Cadastro </Text >
+    <Text>Digite seu nome</Text>
+      <TextInput placeholder='Nome'  style={styles.textinp}/>}
       <Text></Text>
       <Text>Digite seu email:</Text>
       <TextInput placeholder='email'  style={styles.textinp}/>
       <Text></Text>
       <Text>Digite seu CPF:</Text>
-      <TextInput placeholder='CPF'  style={styles.textinp}/>
+  <TextInput placeholder='CPF'  style={styles.textinp}/>
+
       <TouchableOpacity activeOpacity={0.7} style={styles.button}>
-        <Text>Enviar</Text>
-      </TouchableOpacity>
+        <Text>Adicionar tarefas</Text>
+      </TouchableOpacity>*/}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -34,7 +42,7 @@ const styles = StyleSheet.create({
     width:200,
     height:30,
     borderRadius:7,
-    padding:30,
+  
     textDecorationColor:'#4F4F4F',
   },
 button:{
